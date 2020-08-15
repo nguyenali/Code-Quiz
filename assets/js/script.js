67bd0a1442595771c1aa2eda895b2d14fa5e92fe
Ali Nguyen <alin57717@gmail.com>
2020-08-15 15:12:02 -0700

    correct condition statmennt for functions in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,110 +25,108 @@ const questions = [
 ];
 
 
-
-
-
-
-
 $(function () {
 
     let score = $('#score');
-    let end = $ ('#end');
-    let time = $ ('#time');
-    let start = $ ('#start');
-    let question = $ ('#question');
-    let startBtn = $ ('#start-btn');
-    let final = $ ('#final"');
-    let finalSubmit = $ ('#finalSubmit');
-    let finalInput = $ ('#finalInput');
-    let timeCount = $ questions.length * 15;
-    let currentQuestion = 0;
+    let end = $('#end');
+    let time = $('#time');
+    let start = $('#start');
+    let question = $('#question');
+    let startBtn = $('#start-btn');
+    let final = $('#final');
+    let finalSubmit = $('#finalSubmit');
+    let finalInput = $('#finalInput');
+    let timeCount = questions.length * 15;
+    let curentQuestion = 0;
     let interval;
 
 
     //history
 
-    
-    let history = $ ('#history');
-    let back = $ ('#back');
-    let clearHistory = $ ('#clearHistory');
-    let historyArray = []
+    let history = $('#history');
+    let back = $('#back');
+    let clearHistory = $('#clearHistory');
+    let historyArray = [];
     localStorage.setItem('score', JSON.stringify([]));
-    getHistory = () => {
+    getHistory = ()=>{
         $('#score-box').empty();
-        if (localStorage.getItem('score')) {
+        if (localStorage.getItem('score')){
             historyArray = JSON.parse(localStorage.getItem('score'));
             historyArray.sort((a, b) => b.score - a.score);
         }
         historyArray.forEach((item, index) => {
-            let itemscore = $('<div class="score-item">${index+1} <span class="s-score">${item.score}</span></div>');
+            let itemscore = $(`<div class="score-item">${index + 1}. ${item.name} <span class="s-score">${item.score}</span></div>`);
             $('#score-box').append(itemscore);
         })
-
-
     };
 
+    getHistory();
+
+    score.on('click', function () {
+        history.addClass('active');
+    });
+
+    back.on('click', function () {
+        history.removeClass('active');
+    });
+
+    clearHistory.on('click', function () {
+        localStorage.setItem('score', JSON.stringify([]));
+        getHistory();
+    });
+
+
     //end history
+
 
     //start quiz
 
     function startQuiz() {
         start.addClass('fade');
         question.addClass('active');
-        nextQuestion(currentQuestion);
-        }
+        nextQuestion(curentQuestion);
+    }
 
-        function nextQuestion (number) {
+    function nextQuestion(number) {
         if (number <= questions.length-1){
             $('#question-title').text(questions[number].title);
             $('#answer-btn').empty();
 
-            questions[number].choices.forEach((item, index) => {
-                let answer = $ (' <button class-"answer>${index + 1'}. ${item}</button>');
+            questions[number].choices.forEach((item,index) => {
+                let answer = $(`<button class="answer">${index + 1}. ${item}</button>`);
                 $('#answer-btn').append(answer);
-            }
-        });
-
-
-      else {
-        question.removeClass('active');
-        end.addClass('active');
-        clearInterval(interval);
-        if (timeCount<0) {
-            time.text(0) 
-            final.text(0);
+            });
         } else {
-            final.text(timeCount);
-            time.text(timeCount)
-        }
-        }
-
-
-
-
-    }
-
-    $(document).on('click', ',answer', function() {
-        if (this.innerText.slice(3,this.innerText.length)=== questions[currentQuestion].answer)
+            question.removeClass('active');
+            end.addClass('active');
+            clearInterval(interval);
+            if (timeCount < 0) {
+                time.text(0);
+                final.text(0);
+            } else {
+                final.text(timeCount);
+                time.text(timeCount)
+            }
         }
     }
-        else {
+
+    $(document).on('click','.answer', function () {
+        if (this.innerText.slice(3,this.innerText.length) === questions[curentQuestion].answer){
+        } else {
+            errorSound();
             timeCount -= 15;
             if (timeCount < 0) {
                 time.text(0);
                 final.text(0);
             }
         }
-
-        currentQuestion++;
-        nextQuestion(currentQuestion);
-
+        curentQuestion++;
+        nextQuestion(curentQuestion);
     });
-
-    startBtn.on('click', function() {
+    startBtn.on('click', function () {
         startQuiz();
-        interval - setInterval(function() {
-            if (timeCount < = 0) {
+        interval = setInterval(function () {
+            if (timeCount <= 0) {
                 timeCount = 0;
                 return
             }
@@ -139,70 +137,23 @@ $(function () {
 
     //end start quiz
 
+
     //finish
 
-    finalSubmit.on ('click', function() {
-        if (timeCount <0) {
-            timeCount= 0
+    finalSubmit.on('click', function () {
+        if (timeCount < 0) {
+            timeCount = 0
         }
-
-        if (finalInput.val()) {
-            historyArray.push ({name: finalInput.val(), score: timeCount});
+        if (finalInput.val()){
+            historyArray.push({name: finalInput.val(), score: timeCount});
             finalInput.val('');
             localStorage.setItem('score', JSON.stringify(historyArray));
             history.addClass('active');
             getHistory();
         }
-
         end.removeClass('active');
         start.removeClass('fade');
-        currentQuestion = 0;
+        curentQuestion = 0;
         time.text(0);
         timeCount = questions.length * 15;
-
-
-
     });
-
-    //end finish
-    
-    
-    
-    )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
