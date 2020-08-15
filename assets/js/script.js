1e2b701bc58151b1bdd1d4855adb0a2809b30a35
Ali Nguyen <alin57717@gmail.com>
2020-08-14 23:53:42 -0700

    add function define in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 
 $(function () {
 
+    let score = $("#score");
+    let end = $ ("#end");
+    let time = $ ("#time");
+    let start = $ ("#start");
+    let question = $ ("#question");
+    let startBtn = $ ("#start-btn");
+    let final = $ ("#final");
+    let finalSubmit = $ ("#finalSubmit");
+    let finalInput = $ ("#finalInput");
+    let timeCount = $ questions. length * 15;
+    let currentQuestion = 0;
+    let interval;
+    
+
 
 
 
